8a64878622de53479a04fa90d1eab5cc311c1f67
thanhvuong1605 <phucthanhvuong@gmail.com>
2018-02-22 00:08:52 +0200

    change

--- ordersystem/settings.py
+++ ordersystem/settings.py
@@ -141,7 +141,7 @@ DATABASES['default'].update(db_from_env)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
 }
